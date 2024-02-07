5ecb72657110c8b7e7ea0ec07a55a7a4322e8637
KostasDemiris <106978666+KostasDemiris@users.noreply.github.com>
2024-02-07 17:10:37 +0000

    Update BinaryHeap.c

--- Algorithmns/BinaryHeap.c
+++ Algorithmns/BinaryHeap.c
@@ -7,121 +7,168 @@ typedef struct pair{
     int priority;
 } pair;
 
-pair create_new_pair(int value, int priority){
+typedef struct binaryHeap{
+    int max_length; 
+    // constraint, must be greater than or equal to 2, one for empty and one for first value. An empty heap is invalid in my definition
+    int current_length;
+    pair** pairs;
+} binaryHeap;
+
+int min(int one, int two){
+    if (one < two) {return one;}
+    return two;
+}
+
+pair* create_new_pair(int value, int priority){
     pair* new_value;
     new_value = (pair*) malloc(sizeof(pair));
     new_value->value = value;
     new_value->priority = priority;
-    return *new_value;
+    return new_value;
 }
 
-int min(int one, int two){
-    if (one < two){
-        return one;
+void heap_swim(binaryHeap* heap){
+    int index = heap->current_length;
+    while ((int) index/2 >= 1){
+        if (heap->pairs[index]->priority < heap->pairs[(int) index/2]->priority) {return;}
+        pair* temp = heap->pairs[index];
+        heap->pairs[index] = heap->pairs[(int) index/2];
+        heap->pairs[(int) index/2] = temp;
+        index = (int) index/2;
     }
-    return two;
 }
 
-void heap_insert(pair* heap, int size, int value, int priority, int maximum_length){
-    // all the other functions don't have a maximum length, and that's because this function (for the implmentation I went with)
-    // the comparision to the last value's priority and subsequent replacement rather than going for the next value only occurs when 
-    // the maximum length of the binary heap is exceeded, and this was the easiest way to interface it in my opinion
-    int index;
-    if (size != 0 && size >= maximum_length && heap[maximum_length].priority > priority) {return;}
-    else{
-        if (size >= maximum_length) {index = maximum_length;} // if the list is full, and it's got a higher priority than the least prioritised value,
-        // replace that one.
-        else {index = size + 1;}// the element after the current last
-    }
-    
-    pair new_pair = create_new_pair(value, priority);
-    heap[index] = new_pair;
-    while ((int) index/2 > 0){
-        if (heap[(int) index/2].priority > heap[index].priority) { return; } // heap order has been restored
-        pair temp = heap[index];
-        heap[index] = heap[(int) index / 2];
-        heap[(int) index / 2] = new_pair; 
-        index = (int) index / 2;
-    }
-    return;
-}
-
-void heap_sink(pair* heap, int size){
-    pair last = heap[1];
-    int index = 1; 
-    while ((index * 2) < (size)){
-        if (heap[index].priority > heap[index * 2].priority && heap[index].priority > heap[(index * 2) + 1].priority) {return;}
-        if ((index * 2) + 1 >= size){ // If it only has a single left child
-            if (heap[index].priority < heap[(index * 2) + 1].priority) {
-                heap[index] = heap[(index * 2) + 1];
-                heap[index] = last;
-            }
-            return;
+void heap_insert(binaryHeap* heap, int value, int priority){
+    if (heap->current_length == 0) {
+        heap->pairs[1] = create_new_pair(value, priority);
+        heap->current_length++;
         }
+    if (heap->current_length == heap->max_length) {
+        // only insert the value if it has more priority than the least prioritised current element;
+        if (heap->pairs[heap->max_length]->priority < priority){
+            free(heap->pairs[heap->max_length]);
+            heap->pairs[heap->max_length] = create_new_pair(value, priority);
+            heap_swim(heap);
+        }
+        return;
+        
+    }
+    heap->pairs[heap->current_length+1] = create_new_pair(value, priority);
+    heap->current_length++;
+    heap_swim(heap);
+}
 
-        if (heap[index * 2].priority > heap[(index * 2) + 1].priority){
-            heap[index] = heap[index * 2];
-            heap[index] = last;
-            index *= 2;
+void heap_sink(binaryHeap* heap){
+    int index = 1;
+    while (index < heap->current_length){
+        if ((index*2)+1 < heap->current_length){
+            if (heap->pairs[(index*2)+1]->priority < heap->pairs[index]->priority && heap->pairs[index*2]->priority < heap->pairs[index]->priority){
+                // this means the heap order has been restored
+                return;
+            }
+            if (heap->pairs[(index*2)+1]->priority > heap->pairs[index*2]->priority){
+                pair* temp = heap->pairs[index];
+                heap->pairs[index] = heap->pairs[(index*2)+1];
+                heap->pairs[(index*2)+1] = temp;
+                index = (index*2) + 1;
+            }
+            else{
+                if (heap->pairs[(index*2)+1]->priority < heap->pairs[index*2]->priority){
+                    pair* temp = heap->pairs[index];
+                    heap->pairs[index] = heap->pairs[index*2];
+                    heap->pairs[index*2] = temp;
+                    index *= 2;
+                }
+                else{
+                    return;
+                }
+            }
+            
+        }
+        else{
+            if (index*2 < heap->current_length){
+                if (heap->pairs[index*2]->priority < heap->pairs[index]->priority){
+                    // again, restored heap order.
+                    return;
+                }
+                pair* temp = heap->pairs[index];
+                heap->pairs[index] = heap->pairs[index*2];
+                heap->pairs[index*2] = temp;
+                index *= 2;
+            }
+            else{
+                // it's a leaf node so to speak, so naturally order has been restored.
+                return;
+            }
         }
         
-        else{
-            heap[index] = heap[(index * 2) + 1];
-            heap[index] = last;
-            index *= 2;
-            index ++;
-        }
-    }
-    return;
-}
-
-int heap_max_deletion(pair* heap, int size){
-    pair root = heap[1]; pair last = heap[size-1];
-    heap[1] = heap[size-1]; // no need to delete it since it'll be overwritten by any later inserts
-    heap_sink(heap, size-1);
-    return root.value;
-}
-
-void print_heap(pair* heap, int size){
-    for (int index = 1; index < size; index++){
-        printf("for index %d, the value is %d with priority %d. \n", index, heap[index].value, heap[index].priority);
     }
 }
 
-int check_heap_property(pair* heap, int size){
-    for (int index = 1; index < size; index++){
-        if (index * 2 < size && heap[index*2].priority > heap[index].priority) {return 0;}
-        if ((index * 2) + 1 < size && heap[(index*2)+1].priority > heap[index].priority) {return 0;}
+int heap_max_delete(binaryHeap* heap){
+    if (heap->current_length < 1){
+        return NULL;
+    } 
+    int returnable = heap->pairs[1]->value;
+    heap->pairs[1] = heap->pairs[heap->current_length];
+    free(heap->pairs[heap->current_length]);
+    heap->current_length--;
+    heap_sink(heap);
+    return returnable;
+
+}
+
+binaryHeap* create_binary_heap(int maximum_length){
+    binaryHeap* heap;
+    heap = (binaryHeap*) malloc(sizeof(binaryHeap));
+    heap->current_length = 0;
+    heap->max_length = maximum_length;
+    heap->pairs = (pair**) malloc(sizeof(pair*) * maximum_length);
+    return heap;
+}
+
+void print_binary_heap(binaryHeap* heap){
+    for (int index = 1; index < heap->current_length; index++){
+        printf("index %d has value %d and priority %d\n", index, heap->pairs[index]->value, heap->pairs[index]->priority);
+    }
+}
+
+int check_heap_order_property(binaryHeap* heap){
+    // Checks that a heap fulfills the required heap order property
+    for (int index = 1; index < heap->current_length; index++){
+        if (index * 2 < heap->current_length && heap->pairs[index*2]->priority > heap->pairs[index]->priority) {return 0;}
+        if ((index * 2) + 1 < heap->current_length && heap->pairs[(index*2)+1]->priority > heap->pairs[index]->priority) {return 0;}
     }
     return 1;
 }
 
-int check_pair_property(pair* heap, int size){ // checks if all pairs have the same value and priority, just a testing function
-    for (int index = 1; index < size; index++){
-        if (heap[index].priority != heap[index].value) {return 0;}
-    }
+int check_heap_symmetry_property(binaryHeap* heap){
+    // Checks that a heap fulfills the required heap symmetry property
+    for (int index = 1; index < heap->current_length; index++) {if (heap->pairs[index]->priority != heap->pairs[index]->value) {return 0;}}
     return 1;
 }
-
 
 int main(void){
     srand(time(NULL));
+
     int gen_num = 50;
-    int size = 0; 
     int max_length = 20;
-    pair heap[max_length];
+
+
     int input[gen_num];
     for (int index = 0; index < gen_num; index++){
         input[index] = rand() % (gen_num);
         printf("%d  ", input[index]);
     } printf("\n");
 
-    for (int index = 0; index < gen_num; index++){
-        heap_insert(&heap[0], size, input[index], input[index], max_length);
-        size = min((size+1), max_length);
-    }
-    print_heap(heap, size);
-    printf("%d is heap property checker\n", check_heap_property(heap, size));
-    printf("%d is the heap pair equivalence property\n", check_pair_property(heap, size));
+    binaryHeap* heap = create_binary_heap(max_length);
 
+    for (int index = 0; index < gen_num; index++){
+        heap_insert(heap, input[index], input[index]);
+    }
+
+    print_binary_heap(heap);
+    printf("order property is %d\n", check_heap_order_property(heap));
+    printf("symmetry property is %d\n", check_heap_symmetry_property(heap));
+    printf("the dequeued value is %d and the length is now %d", heap_max_delete(heap), heap->current_length);
 }
